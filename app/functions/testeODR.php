61daf698f6edb4b964175d71e4e29fe4753ff3e0
odairsp <odair.sp@hotmail.com>
2023-07-26 12:16:44 -0300

    att

--- app/functions/testeODR.php
+++ app/functions/testeODR.php
@@ -1,6 +1,29 @@
 <?php
 
 
-for ($i=0;$i <= 100; $i++) { 
-   echo $i % 2 != 0 ? $i :"\n";
+// for ($i=0;$i <= 100; $i++) { 
+//    echo $i % 2 != 0 ? $i :"\n";
+// }
+
+
+
+//var_dump (!!0 ?'zero':'um');
+
+function name()
+{
+   return 'NOME';
+
 }
+
+function lastname()
+{
+   return 'SOBRENOME';
+}
+
+function completo($callback1)
+{
+
+   return $callback1();
+}
+
+echo completo('lastname');
