f7250a44d9afc5658c85464c9bcd6b98132030cd
odairsp <odair.sp@hotmail.com>
2023-07-27 11:59:41 -0300

    att

--- public/index.php
+++ public/index.php
@@ -10,12 +10,18 @@ require "../bootstrap.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-
-    <title>Curso PHP</title>
+    <title>Udemy PHP </title>
 </head>
 
 <body>
-    <header>Header</header>
+
+    <header>
+        <div class="container-fluid">
+            Header
+        </div>
+
+    </header>
+
     <main>
         <div class="container">
 
@@ -35,7 +41,11 @@ require "../bootstrap.php";
     </main>
 
 
-    <footer>Footer</footer>
+    <footer>
+        <div class="container-fluid">
+            Footer
+        </div>
+    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
     </script>
